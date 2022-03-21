0dec4096a8261dd699240e095bea03cf38a64e20
xzor9 <alexandrep@gmail.com>
2022-03-21 09:59:09 -0400

    new answers

---
+++ 8kyu/Return Negative.js
@@ -0,0 +1,12 @@
+function makeNegative(num) {
+  if(num > 0){
+    return -num;
+  }
+  else{
+    return num;
+  }
+}
+
+//function makeNegative(num) {
+//  return num < 0 ? num : -num;
+//}
---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,21 @@
+function solution(str){
+      var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+
+
+
+//function solution(str){
+//  return str.split('').reverse().join('');  
+//}
