6e1ffeeea3ab2675b5cf67ee015ab7bb9ab51ecb
xzor9 <alexandrep@gmail.com>
2022-03-27 21:41:25 -0400

    new kata

---
+++ 8kyu/Is n divisible by x and y.js
@@ -0,0 +1,13 @@
+function isDivisible(n, x, y) {
+  if(n%x===0 && n%y===0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+//function isDivisible(n, x, y) {
+//  return (n % x === 0 && n % y === 0) ? true : false;
+//}
