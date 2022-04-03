9b1194569f11a80306d02d268293e063f2884163
xzor9 <alexandrep@gmail.com>
2022-04-03 11:11:01 -0400

    new katas

---
+++ 8kyu/Beginner Series #1 School Paperwork.js
@@ -0,0 +1,12 @@
+function paperwork(n, m) {
+  if(n<0 || m<0){
+    return 0;
+  } else{
+    return n*m;
+  }
+}
+
+
+//function paperwork(n, m) {
+//  return n > 0 && m > 0 ? n * m : 0
+//}
---
+++ 8kyu/DNA to RNA Conversion.js
@@ -0,0 +1,5 @@
+function DNAtoRNA(dna) {
+  return dna.split('T').join('U');
+}
+
+//const DNAtoRNA = dna => dna.replace(/T/g, 'U');
