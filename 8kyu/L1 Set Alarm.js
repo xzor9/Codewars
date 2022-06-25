45c6f2a725f501dbe78b09046381c1ca88e67349
xzor9 <alexandrep@gmail.com>
2022-06-25 11:29:01 -0400

    new kata

---
+++ 8kyu/L1 Set Alarm.js
@@ -0,0 +1,11 @@
+function setAlarm(employed, vacation){
+  if(employed===true && vacation===false){
+    return true
+  }else{
+    return false
+  }
+}
+
+//function setAlarm(employed, vacation){
+//  return employed && !vacation;
+//}
